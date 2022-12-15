99e247e19a9179486479b4af6f7651e351751e5d
Dan Shield <dshield@unomaha.edu>
2022-12-14 22:32:40 -0600

    Adding comments

--- article.py
+++ article.py
@@ -19,19 +19,21 @@ class MyArticle():
         self.url = url
         self.publisher = publisher
 
-        self.entities = []
-        self.doc = None
-        self.error = ""
         self.dates = []
+        self.entities = []
+        self.doc = None    # Useful for debugging
+        self.error = ""    # Useful for debugging
         
         self.sutime = self._init_parsers()
 
     
+    # Set up the SUTime object
     def _init_parsers(self):
         jar_files = os.path.join(os.path.dirname(__file__), 'jars')
         return SUTime(jars=jar_files, mark_time_ranges=True)
 
         
+    # String representation for debugging
     def __repr__(self):
         tmp = f'Title:        {self.title}\r\n'
         tmp += f'Description: {self.description}\r\n'
@@ -42,30 +44,36 @@ class MyArticle():
         return tmp
 
     
+    # Clean up text when necessary
     def clean_text(self, e):
         tmp = e.text.replace('\n','').replace('the ', '').replace('The ', '')
 
         return tmp
     
     
+    # Only select the labels we are interested in
     def filter_label(self, e):
         if (e.label_ == 'GPE'):
             return True
         elif (e.label_ == 'PERSON'):
-            if (len(e.text.split(' ')) > 1):
+            if (len(e.text.split(' ')) > 1): #include first and last names
                 return True
         else:
             return False
         
     
+    # Get the list of DATE entities
     def get_dates(self, str):
+        # Get the dates
         dates = self.sutime.parse(str)
         dates = [x['value'] for x in dates]
         
+        # Put them in the string format we need
         dtFormat = '\d{4}-\d{2}-\d{2}'
         return  [dateparser.parse(d).strftime('%Y-%m-%d') for d in dates if (re.search(dtFormat, d))]
         
 
+    # Download, parse, and collect entities from article text
     def Init(self):
         user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
         config = Config()
--- news.py
+++ news.py
@@ -3,17 +3,22 @@ from gnews import GNews
 import numpy as np
 import pandas as pd
 
+# Get list of articles to process
 request = GNews(max_results=100)
 articles = [MyArticle(request, a['title'], a['description'], a['published date'], a['url'], a['publisher']['title']) for a in request.get_news('+"Wagner group"')]
 
+# Initialize all the objects
 [a.Init() for a in articles]
 
+# Create list of entities from all articles
 merged = list()
 [merged.extend(a.entities) for a in articles if (len(a.entities) > 0)]
 unique = np.unique(merged)
 
+# Create columns from the all but the least frequently used entities
 columns = [e for e in unique if (merged.count(e) > (len(articles)*0.025))]
 
+# Build the dataframe with articles as rows and entities and columns
 df = pd.DataFrame([(a.publisher, a.title, a.url, a.date) for a in articles], columns = ['Publisher','Title','URL','Date'])
 for column in columns:
     list = []
@@ -25,4 +30,5 @@ for column in columns:
             
     df[column] = list
 
+# Save the results
 df.to_csv("Econ8320_Project.csv")
